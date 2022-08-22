c3198ec149bb4d53d28c52c399c41ccc59dce87a
Sam McLeod <sammcj@users.noreply.github.com>
2022-08-22 16:43:04 +1000

    Cleanup output

--- index.js
+++ index.js
@@ -8,38 +8,40 @@ import Chalk from 'chalk';
 import simpleGit from 'simple-git';
 import process from 'process';
 
-// const head = core.getInput('head', {
-//   required: true,
-//   description: 'The name of the branch to compare against',
-//   default: 'main',
-// });
-// const feature = core.getInput('feature', {
-//   required: true,
-//   description: 'The feature branch to compare against',
-//   default: 'dev',
-// });
-// const path = core.getInput('path', {
-//   required: false,
-//   description: 'Path to compare, defaults to CWD',
-//   default: './',
-// });
-// const similarity = core.getInput('similarity', {
-//   required: true,
-//   description: 'similarity (50 = 50%)',
-//   default: '50',
-// });
-// const diffFilter = core.getInput('mode', {
-//   required: false,
-//   description: 'Check for modified or renamed files (R|M|A|C|D|T|U|X|B|*), defaults to R (renamed)',
-//   default: 'R',
-// });
+const head = core.getInput('head', {
+  required: true,
+  description: 'The name of the branch to compare against',
+  default: 'main',
+});
+const feature = core.getInput('feature', {
+  required: true,
+  description: 'The feature branch to compare against',
+  default: 'dev',
+});
+const path = core.getInput('path', {
+  required: false,
+  description: 'Path to compare, defaults to CWD',
+  default: './',
+});
+const similarity = core.getInput('similarity', {
+  required: true,
+  description: 'similarity (50 = 50%)',
+  default: '50',
+});
+const diffFilter = core.getInput('mode', {
+  required: false,
+  description: 'Check for modified or renamed files (R|M|A|C|D|T|U|X|B|*), defaults to R (renamed)',
+  default: 'R',
+});
+
+process.env.GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE || process.cwd();
 
 // For stubbing purposes
-const feature = 'dev'
-const path = './';
-const head = 'main'
-const similarity = '50'
-const diffFilter = 'M'
+// const feature = 'dev'
+// const path = './';
+// const head = 'main'
+// const similarity = '50'
+// const diffFilter = 'M'
 
 async function run() {
   try {
@@ -56,8 +58,8 @@ async function run() {
     );
 
     // fetch both refs
-    // await git.fetch(head);
-    // await git.fetch(feature);
+    await git.fetch(head);
+    await git.fetch(feature);
 
     // diff two git branches for renamed files in the given path
     const diff = await git.diff([
