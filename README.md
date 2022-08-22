1538cdf8eb5244e33f206e3ffcbab200509e8077
Sam McLeod <sammcj@users.noreply.github.com>
2022-08-22 10:22:34 +1000

    A new start

---
+++ README.md
@@ -0,0 +1,32 @@
+# Check Renamed Files
+
+This action checks for renamed files between two git refs.
+
+## Inputs
+
+- `head`: The first ref to check.
+- `feature`: The second ref to check.
+- `path`: The path to the file to check.
+- `similarity`: The similarity threshold to use when comparing files, is treated as a percentage (e.g. 50 = 50%).
+
+## Usage
+
+```yaml
+name: Check for renamed files
+on:
+  merge:
+    branches:
+      - main
+
+jobs:
+  check-renamed-files:
+      - uses: actions/checkout@v3
+        with:
+          fetch-depth: "0"
+      - uses: sammcj/check-renamed-files-action@main
+        with:
+          head: main
+          feature: dev
+          path: src/main/resources/db/migration
+          similarity: 50
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+/* eslint-disable max-params */
+/* eslint-disable no-inner-declarations */
+import core from '@actions/core';
+// eslint-disable-next-line import/no-named-as-default
+import Chalk from 'chalk';
+// eslint-disable-next-line import/no-named-as-default
+import simpleGit from 'simple-git';
+
+const head = core.getInput('head', {
+  required: true,
+  description: 'The name of the branch to compare against',
+  default: 'main',
+});
+const feature = core.getInput('feature', {
+  required: true,
+  description: 'The feature branch to compare against',
+  default: 'dev',
+});
+const path = core.getInput('path', {
+  required: true,
+  description: 'Path to check for changes',
+  default: 'src/main/resources/db/migration/',
+});
+const similarity = core.getInput('similarity', {
+  required: true,
+  description: 'similarity (50 = 50%)',
+  default: '50',
+});
+
+// For stubbing purposes
+// const feature = 'dev'
+// const path = 'src/main/resources/db/migration/'
+// const head = 'main'
+// const similarity = '50'
+
+async function run() {
+  try {
+
+    const git = simpleGit();
+
+    console.log(
+      Chalk.green('[ Comparing HEAD:'),
+      Chalk.bgGreen.bold(head),
+      Chalk.green('and FEATURE:'),
+      Chalk.bgBlue.bold(feature),
+      Chalk.green('in PATH:'),
+      Chalk.bgMagenta.bold(path),
+      Chalk.green(']\n'),
+    );
+
+    // fetch both branches
+    await git.fetch(head);
+    await git.fetch(feature);
+
+    // diff two git branches and print only the file names
+    const diff = await git.diff(['--diff-filter=R', `--find-renames=${similarity}%`, head, '--', path, feature, '--', path]);
+    console.log(diff);
+
+    const modifiedFiles = diff.split('\n');
+
+    if (modifiedFiles.length > 0) {
+      console.log(Chalk.red('Renamed files:\n'));
+      console.log(Chalk.red(modifiedFiles));
+      core.setFailed('ERROR: Renamed files found!');
+    } else {
+      console.log(Chalk.green('No renamed files found in the path\n'));
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
