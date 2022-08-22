0ea9a51593e8b65c958f8d7e9b43c85ef7b26e90
Sam McLeod <sammcj@users.noreply.github.com>
2022-08-22 10:35:14 +1000

    Cleanup output

--- README.md
+++ README.md
@@ -4,10 +4,15 @@ This action checks for renamed files between two git refs.
 
 ## Inputs
 
-- `head`: The first ref to check.
-- `feature`: The second ref to check.
-- `path`: The path to the file to check.
-- `similarity`: The similarity threshold to use when comparing files, is treated as a percentage (e.g. 50 = 50%).
+- `head`: The first ref to check (defaults to main).
+- `feature`: The second ref to check (defaults to dev).
+- `path`: The path to the file to check (defaults to working directory).
+- `similarity`: The similarity threshold to use when comparing files, is treated as a percentage (defaults to 50).
+- `diffFilter`: The git diff filter to use when comparing files (R|M|A|C|D|T|U|X|B|*), defaults to R (renamed).
+
+## Outputs
+
+- `modifiedFiles`: An array of files that were renamed between the two refs.
 
 ## Usage
 
@@ -20,13 +25,14 @@ on:
 
 jobs:
   check-renamed-files:
-      - uses: actions/checkout@v3
-        with:
-          fetch-depth: "0"
-      - uses: sammcj/check-renamed-files-action@main
-        with:
-          head: main
-          feature: dev
-          path: src/main/resources/db/migration
-          similarity: 50
+    - uses: actions/checkout@v3
+      with:
+        fetch-depth: '0'
+    - uses: sammcj/check-renamed-files-action@main
+      with:
+        head: main
+        feature: dev
+        path: src/main/resources/db/migration
+        similarity: 50
+        diffFilter: R
 ```
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 /* eslint-disable max-params */
 /* eslint-disable no-inner-declarations */
-import core from '@actions/core';
+import core, { ExitCode } from '@actions/core';
 // eslint-disable-next-line import/no-named-as-default
 import Chalk from 'chalk';
 // eslint-disable-next-line import/no-named-as-default
 import simpleGit from 'simple-git';
+import process from 'process';
 
 const head = core.getInput('head', {
   required: true,
@@ -18,25 +19,30 @@ const feature = core.getInput('feature', {
   default: 'dev',
 });
 const path = core.getInput('path', {
-  required: true,
-  description: 'Path to check for changes',
-  default: 'src/main/resources/db/migration/',
+  required: false,
+  description: 'Path to compare, defaults to CWD',
+  default: './',
 });
 const similarity = core.getInput('similarity', {
   required: true,
   description: 'similarity (50 = 50%)',
   default: '50',
 });
+const diffFilter = core.getInput('mode', {
+  required: false,
+  description: 'Check for modified or renamed files (R|M|A|C|D|T|U|X|B|*), defaults to R (renamed)',
+  default: 'R',
+});
 
 // For stubbing purposes
 // const feature = 'dev'
-// const path = 'src/main/resources/db/migration/'
+// const path = './;
 // const head = 'main'
 // const similarity = '50'
+// const diffFilter = 'R'
 
 async function run() {
   try {
-
     const git = simpleGit();
 
     console.log(
@@ -49,28 +55,39 @@ async function run() {
       Chalk.green(']\n'),
     );
 
-    // fetch both branches
+    // fetch both refs
     await git.fetch(head);
     await git.fetch(feature);
 
-    // diff two git branches and print only the file names
-    const diff = await git.diff(['--diff-filter=R', `--find-renames=${similarity}%`, head, '--', path, feature, '--', path]);
-    console.log(diff);
+    // diff two git branches for renamed files in the given path
+    const diff = await git.diff([
+      '--name-only',
+      `--diff-filter=${diffFilter}`,
+      `--find-renames=${similarity}%`,
+      head,
+      feature,
+      '--',
+      `${process.env.GITHUB_WORKSPACE}/${path}`,
+    ]);
 
-    const modifiedFiles = diff.split('\n');
+    const diffClean = diff.split(/\r?\n/) // Split input text into an array of lines
+      .filter(line => line.trim() !== '') // Filter out lines that are empty or contain only whitespace
+      .join('\n'); // Join line array into a string
 
-    if (modifiedFiles.length > 0) {
-      console.log(Chalk.red('Renamed files:\n'));
-      console.log(Chalk.red(modifiedFiles));
-      core.setFailed('ERROR: Renamed files found!');
+    const modifiedFiles = diffClean.split('\n');
+    const modifiedFilesArray = modifiedFiles.map((file) => file.split('\n'));
+
+    if (modifiedFiles.length > 1) {
+      core.setOutput(`modified files with filter ${diffFilter} found in ${path}:`, modifiedFilesArray);
+      console.log(modifiedFilesArray);
+      core.setFailed(`ERROR: ${modifiedFiles.length} Modified files with filter ${diffFilter} found in ${path} !`);
+      ExitCode.Failure;
     } else {
-      console.log(Chalk.green('No renamed files found in the path\n'));
+      console.log(Chalk.green(`No modified files with filter ${diffFilter} found in ${path}\n`));
     }
-
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
-
