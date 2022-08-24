5c1ff4778d2945b654f94d429d6475a4a4b67fc1
Sam McLeod <sammcj@users.noreply.github.com>
2022-08-24 14:44:02 +1000

    testing

--- index.js
+++ index.js
@@ -141,6 +141,32 @@ async function run() {
     const modifiedFiles = diff.trim().split('\n').filter(file => file !== '');
 
     if (modifiedFiles.length > 0) {
+      // check if the modified files from the diff were renamed
+      const renamedDiff = await git.diff([
+        '--name-only',
+        '--diff-filter=R',
+        `--find-renames=${similarity}%`,
+        `--find-copies=${similarity}%`,
+        head,
+        feature,
+        '--',
+        searchPath,
+      ]);
+      // Clean up modified files to ensure no false positives with empty lines
+      const renamedFiles = renamedDiff.trim().split('\n').filter(file => file !== '');
+      if (renamedFiles.length > 0) {
+        const errorString = `ERROR ${modifiedFiles.length} modified files with filter ${diffFilter} found in ${searchPath} !`
+        core.setFailed(errorString);
+        ExitCode.Failure;
+        console.log(Chalk.red(
+          errorString,
+          '\n',
+          'Files:',
+          Chalk.bgRedBright(
+            modifiedFiles)));
+        core.setFailed(errorString);
+        ExitCode.Failure;
+      }
 
       if (checkFileNameDates) {
         // Extract the date from the filenames for each modified file (e.g. V2022.02.02.2024__my_db_migration_abc.sql)
@@ -180,24 +206,10 @@ async function run() {
             console.log(Chalk.green('No modified files have names older than files on the head branch\n'));
           }
         }
+      } else {
+        console.log(Chalk.green(`No modified files with filter ${diffFilter} found in ${searchPath}\n`));
       }
-
-      const errorString = `ERROR ${modifiedFiles.length} modified files with filter ${diffFilter} found in ${searchPath} !`
-      core.setFailed(errorString);
-      ExitCode.Failure;
-      console.log(Chalk.red(
-        errorString,
-        '\n',
-        'Files:',
-        Chalk.bgRedBright(
-          modifiedFiles)));
-      core.setFailed(errorString);
-      ExitCode.Failure;
-    } else {
-      console.log(Chalk.green(`No modified files with filter ${diffFilter} found in ${searchPath}\n`));
     }
-
-
 
   } catch (error) {
     core.setFailed(error.message);
